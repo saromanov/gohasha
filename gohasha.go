79f584aed3b089e5b0f69c057114dfe480641256
Sergey Romanov <xxsmotur@gmail.com>
2015-06-23 21:53:36 +0500

    add adler32

--- gohasha.go
+++ gohasha.go
@@ -1,14 +1,16 @@
 package gohasha
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/hex"
 	"errors"
+	"hash/adler32"
 	"hash/crc64"
-	"io/ioutil"
 	"io"
-	"bytes"
+	"io/ioutil"
 	"net/http"
+	"fmt"
 )
 
 type GohashaOptions struct {
@@ -21,7 +23,7 @@ type GohashaOptions struct {
 
 	//Get data from webpage
 	Webpage string
-	
+
 	//Now, algorithms can be md5 or crc32
 	Algorithm string
 }
@@ -64,6 +66,8 @@ func GoHasha(opt *GohashaOptions) (string, error) {
 func crypt(data string, algorithm string) string {
 	if algorithm == "crc64" {
 		return crc64run(data)
+	} else if algorithm == "adler32" {
+		return fmt.Sprint(adler32run(data))
 	} else {
 		return md5run(data)
 	}
@@ -79,4 +83,8 @@ func crc64run(data string) string {
 	hasher := crc64.New(crc64.MakeTable(crc64.ISO))
 	hasher.Write([]byte(data))
 	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func adler32run(data string) uint32 {
+	return adler32.Checksum([]byte(data))
 }
