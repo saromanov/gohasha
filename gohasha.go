c3040e6015e92a986f4ddd89fe0c348ed89830a8
Sergey Romanov <xxsmotur@gmail.com>
2015-06-30 23:54:01 +0500

    Refactoring of GoHasha method

--- gohasha.go
+++ gohasha.go
@@ -5,12 +5,12 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"hash/adler32"
 	"hash/crc64"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"fmt"
 )
 
 type GohashaOptions struct {
@@ -33,22 +33,23 @@ type GohashaOptions struct {
 
 //GoHasha return hash in string from string data or file
 func GoHasha(opt *GohashaOptions) (string, error) {
-	if opt.Data != "" {
+	data := opt.Data
+	/*if opt.Data != "" {
 		return crypt(opt.Data, opt.Algorithm), nil
-	}
+	}*/
 	if opt.Filepath != "" {
-		data, err := ioutil.ReadFile(opt.Filepath)
+		datab, err := ioutil.ReadFile(opt.Filepath)
 		if err != nil {
 			return "", err
 		} else {
-			return crypt(string(data), opt.Algorithm), nil
+			data = string(datab)
 		}
 	}
 
 	if opt.BufferReader != nil {
 		buff := new(bytes.Buffer)
 		buff.ReadFrom(opt.BufferReader)
-		return crypt(buff.String(), opt.Algorithm), nil
+		data = buff.String()
 	}
 
 	if opt.Webpage != "" {
@@ -61,13 +62,21 @@ func GoHasha(opt *GohashaOptions) (string, error) {
 		if err != nil {
 			panic(err)
 		}
-		return crypt(string(result), opt.Algorithm), nil
+		data = string(result)
 	}
-	return "", errors.New("Can't find data for hashing")
+
+	if data == "" {
+		return "", errors.New("Can't find data for hashing")
+	} else {
+		return crypt(data, opt.Algorithm, opt.HashingFunc), nil
+	}
 }
 
 //Choose algorithms for crypt
-func crypt(data string, algorithm string) string {
+func crypt(data string, algorithm string, hashfunc func(string) string) string {
+	if hashfunc != nil {
+		return hashfunc(data)
+	}
 	if algorithm == "crc64" {
 		return crc64run(data)
 	} else if algorithm == "adler32" {
